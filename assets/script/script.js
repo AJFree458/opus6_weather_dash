a7c5e429fe27d7370273309f675dc83850e70f57
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-02 00:19:11 -0500

    Complete code

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,7 +11,7 @@ $.ajax({
 .then(function(response){
     console.log(response)
 }) */
-// Local Storage saved
+//Local Storage saved
 var savedCities = [];
 var currentCity;
 
@@ -51,11 +51,12 @@ function showPrev() {
 }
 
 function getCurrent(city) {
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=7eec630e32e425a54546a905cc476a3a&units=imperial";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7eec630e32e425a54546a905cc476a3a&units=imperial";
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function(response){
+        // console.log(response);
         // Create the card
         var currCard = $("<div>").attr("class", "card bg-light");
         $("#currentCityWeather").append(currCard);
@@ -67,7 +68,7 @@ function getCurrent(city) {
         var cardRow = $("<div>").attr("class", "row no-gutters");
         currCard.append(cardRow);
 
-        //get icons for weather conditions
+        // get icons for weather conditions
         var iconURL = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
 
         var imgDiv = $("<div>").attr("class", "col-md-4").append($("<img>").attr("src", iconURL).attr("class", "card-img"));
@@ -119,8 +120,77 @@ function getCurrent(city) {
     });
 }
 
+// Retrieve the forecast
 function getForecast(city) {
-    // Retrieve the 5 Day Forecast
+    // Retrieve the 5 Day Forecast using the API
     var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&appid=7eec630e32e425a54546a905cc476a3a&units=imperial";
-    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        // Make a container div for the forecast cards
+        var newRow = $("<div>").attr("class", "forecast");
+        $("#currentCityWeather").append(newRow);
+
+        // Loop through the response array for the forecasts for 15:00
+        for (var i = 0; i < response.list.length; i++) {
+            if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                var newCol = $("<div>").attr("class", "one-fifth");
+                newRow.append(newCol);
+
+                var newCard = $("<div>").attr("class", "card text-white bg-primary");
+                newCol.append(newCard);
+
+                var cardImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png");
+                newCard.append(cardImg);
+
+                var bodyDiv = $("<div>").attr("class", "card-body");
+                newCard.append(bodyDiv);
+
+                bodyDiv.append($("<p>").attr("class", "card-text").html("Temp: " + response.list[i].main.temp + " &#8457;"));
+                bodyDiv.append($("<p>").attr("class", "card-text").text("Humidity: " + response.list[i].main.humidity + "%"));
+            }
+        }
+    });
 }
+
+function clear() {
+    //Clear the weather
+    $("#currentCityWeather").empty();
+}
+
+function saveCity(loc) {
+    if (savedCities === null) {
+        savedCities = [loc];
+    }
+    // Save the array to local storage
+    localStorage.setItem("weathercities", JSON.stringify(savedCities));
+    showPrev();
+}
+
+$("#searchBtn").on("click", function() {
+    // Prevent refresh
+    event.preventDefault();
+    // Retrieve the input value
+    var loc = $("#searchCity").val().trim();
+    // If the loc var has something
+    if (loc !== "") {
+        // Clear the forecast
+        clear();
+        currentCity = loc;
+        saveCity(loc);
+        // Clear the search field
+        $("#searchCity").val("");
+        // Retrieve the new forecast
+        getCurrent(loc);
+    }
+});
+
+$(document).on("click", "#loc-btn", function() {
+    clear();
+    currentCity = $(this).text();
+    showPrev();
+    getCurrent(currentCity);
+});
+
+initialize();
